7b2429cd9f5b5ce60444263c0c4d03ba5c5e64e8
MikhailChuza <148324632+MikhailChuza@users.noreply.github.com>
2024-02-27 11:57:27 +0200

    1-2-5

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
+
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -27,3 +29,19 @@ const images = [
     alt: 'Zebras on Zebra',
   },
 ];
+
+
+ const gallery = document.querySelector('.gallery');
+
+  images.forEach(image => {
+    const li = document.createElement('li');
+    li.classList.add('gallery-item');
+
+    const img = document.createElement('img');
+    img.src = image.url;
+    img.alt = image.alt;
+    img.width = 300;
+
+    li.appendChild(img);
+    gallery.appendChild(li);
+  });
